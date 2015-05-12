20027a0898b9bb263c6b13b550a942c793029ddc
isuruw <isuruw@hsenidmobile.com>
2015-05-12 18:19:11 +0530

    Default values added for annotations

--- src/main/java/hms/tap/ussd/manager/annotations/Config.java
+++ src/main/java/hms/tap/ussd/manager/annotations/Config.java
@@ -1,7 +1,5 @@
 package hms.tap.ussd.manager.annotations;
 
-import hms.tap.api.ussd.OperationType;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,5 +9,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 public @interface Config {
     String id();
-    boolean end();
+    boolean end() default true;
 }
